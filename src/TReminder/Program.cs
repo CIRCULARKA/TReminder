59ad1dedccfa5fc804680832a912275fb224605f
CIRCULARKA <donkardon@list.ru>
2021-12-20 15:02:59 +0500

    Add base infrastracure

--- src/TReminder/Program.cs
+++ src/TReminder/Program.cs
@@ -1,11 +1,99 @@
 ﻿using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace TReminder
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly string _pathToLogsFile = "logs";
+
+        static async Task Main(string[] args)
         {
+            var apiKey = JsonSerializer.Deserialize<AppConfiguration>(
+                System.IO.File.ReadAllText("config.json"),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            ).ApiKey;
+
+            var client = new TelegramBotClient(apiKey);
+
+            await ConfigureBotCommandsAsync(client);
+
+            using var cts = new CancellationTokenSource();
+
+            var receiverOptions = new ReceiverOptions {  };
+
+            client.StartReceiving(
+                HandleUpdate,
+                HandleError,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            Console.ReadLine();
+        }
+
+        public static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            if (update.Type != UpdateType.Message)
+                return;
+
+            if (update.Message!.Type != MessageType.Text)
+                return;
+
+            var incomingChatId = update.Message.Chat.Id;
+            var incomingMessage = update.Message.Text;
+
+            var upcomingMessage = update.Message.Text.Length >= 4096 ? "Your message is too long" :
+                $"You sent me the \"{incomingMessage}\" message";
+
+            await client.SendTextMessageAsync(
+                chatId: incomingChatId,
+                text: upcomingMessage,
+                cancellationToken: token
+            );
+        }
+
+        public static Task HandleError(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            System.IO.File.AppendAllLines(
+                $"logs/{DateTime.Now}.txt",
+                new string[] {
+                    $"[{DateTime.Now.ToString("HH:mm MMMM dd, yyyy")}]",
+                    $"Message: {exception.Message}",
+                    $"Assembly: {exception.Source}",
+                    $"Method: {exception.TargetSite}",
+                    exception.InnerException == null ? "No inner exception" : $"Inner exception message: {exception.InnerException.Message}",
+                    $"Stack trace:\n {exception.StackTrace}",
+                    "\n"
+                }
+            );
+
+            Console.WriteLine($"Something went wrong: {exception.ToString()}");
+
+            return Task.CompletedTask;
+        }
+
+        private async static Task ConfigureBotCommandsAsync(ITelegramBotClient client)
+        {
+            var englishCommands = new BotCommand[] {
+                new BotCommand { Command = "new", Description = "Create new reminder" },
+                new BotCommand { Command = "list", Description = "List all reminders you have" },
+            };
+
+            var russianCommands = new BotCommand[] {
+                new BotCommand { Command = englishCommands[0].Command, Description = "Создать новое напомнинание" },
+                new BotCommand { Command = englishCommands[1].Command, Description = "Список имеющихся напоминаний" }
+            };
+
+            await client.SetMyCommandsAsync(englishCommands, languageCode: "en");
+            await client.SetMyCommandsAsync(russianCommands, languageCode: "ru");
         }
     }
 }
