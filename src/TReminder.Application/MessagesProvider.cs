6c29af88300c397e61bca659a2326f06e4322f85
CIRCULARKA <donkardon@list.ru>
2021-12-20 19:40:16 +0500

    Add new constructor

--- src/TReminder.Application/MessagesProvider.cs
+++ src/TReminder.Application/MessagesProvider.cs
@@ -8,19 +8,32 @@ namespace TReminder.Application
     {
         private Messages _messages;
 
+        /// <summary>
+        /// Gets messages from messages.{langCode}.json from root of the assembly by default
+        /// </summary>
         public MessagesProvider(string langCode)
+        {
+            Messages = JsonSerializer.Deserialize<Messages>(
+                File.ReadAllText($"messages.{langCode}.json")
+            );
+        }
+
+        public MessagesProvider(string langCode, StreamReader reader)
+        {
+            Messages = JsonSerializer.Deserialize<Messages>(
+                reader.ReadToEnd()
+            );
+        }
+
+        public Messages Messages { get; init; }
+
+        private void ValidateLanguageCode(string langCode)
         {
             var exception = new ArgumentException("Invalid language code");
 
             if (string.Compare(langCode, "ru", ignoreCase: true) != 0 &&
                 string.Compare(langCode, "en", ignoreCase: true) != 0)
                 throw exception;
-
-            Messages = JsonSerializer.Deserialize<Messages>(
-                File.ReadAllText($"messages.{langCode}.json")
-            );
         }
-
-        public Messages Messages { get; init; }
     }
 }
