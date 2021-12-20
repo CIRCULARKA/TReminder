8b59dc2c689e96c3aa47276c1ab088578c88a3a1
CIRCULARKA <donkardon@list.ru>
2021-12-21 00:05:35 +0500

    Move message provider to special folder

--- src/TReminder/Program.cs
+++ src/TReminder/Program.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Configuration;
 using System.IO;
 using System.Collections.Generic;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
