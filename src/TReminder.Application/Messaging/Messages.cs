35162ef656aa5ff41d213bfbac2ad45f8e655b5c
CIRCULARKA <donkardon@list.ru>
2021-12-21 02:21:42 +0500

    Add new message to Messages class

--- src/TReminder.Application/Messaging/Messages.cs
+++ src/TReminder.Application/Messaging/Messages.cs
@@ -2,6 +2,8 @@ namespace TReminder.Application.Messaging
 {
     public class Messages
     {
+        public string SomethingWentWrong { get; init; }
+
         public string YouSentTheMessage { get; init; }
 
         public string YourMessageIsTooLong { get; init; }
